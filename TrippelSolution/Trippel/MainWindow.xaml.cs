ec661d7d675bd4eab352fb26d805780ba7d457f9
Fernand Elisee Moudio Moudio <fernandmoudio1@gmail.com>
2020-03-17 05:13:51 +0300

    first version of app is ready to use

--- TrippelSolution/Trippel/MainWindow.xaml.cs
+++ TrippelSolution/Trippel/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+//using System.Windows.Forms;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Mail;
 
 namespace Trippel
 {
@@ -25,16 +27,6 @@ namespace Trippel
             InitializeComponent();
             cmbFontFamily.ItemsSource = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
             cmbFontSize.ItemsSource = new List<double>() { 8, 9, 10, 11, 12, 13, 14, 16, 18, 20, 22, 24, 26, 28, 30, 36, 48, 72 };
-        }
-
-        private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-
-        }
-
-        private void Save_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-
         }
 
         private void rtbEditor_SelectionChanged(object sender, RoutedEventArgs e)
@@ -55,12 +47,59 @@ namespace Trippel
         private void cmbFontFamily_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (cmbFontFamily.SelectedItem != null)
-                rtbEditor.Selection.ApplyPropertyValue(Inline.FontFamilyProperty, cmbFontFamily.SelectedItem);
+                rtbEditor.Selection.ApplyPropertyValue (Inline.FontFamilyProperty, cmbFontFamily.SelectedItem);
         }
 
         private void cmbFontSize_TextChanged(object sender, TextChangedEventArgs e)
         {
-            rtbEditor.Selection.ApplyPropertyValue(Inline.FontSizeProperty, cmbFontSize.Text);
+            try
+            {
+                rtbEditor.Selection.ApplyPropertyValue(Inline.FontSizeProperty, cmbFontSize.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
+
+        private void btnColor_Click (object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.ColorDialog colorDialog = new System.Windows.Forms.ColorDialog();
+            if (colorDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                rtbEditor.Selection.ApplyPropertyValue(Inline.ForegroundProperty, new SolidColorBrush(Color.FromArgb(colorDialog.Color.A, colorDialog.Color.R, colorDialog.Color.G, colorDialog.Color.B)));
+            }
+        }
+
+        private void btnSendEmail_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+
+                mail.From = new MailAddress(tbSender.Text);
+                mail.To.Add(tbRecipient.Text);
+                mail.Subject = tbSubject.Text;
+                mail.Body = ExtractContentFromRichTextBox(rtbEditor);
+
+                smtpClient.Credentials = new System.Net.NetworkCredential("fernandmoudio1@gmail.com", "emiwx81990");
+                smtpClient.EnableSsl = true;
+
+                smtpClient.Send(mail);
+                MessageBox.Show("mail Send");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private string ExtractContentFromRichTextBox(RichTextBox rtb)
+        {
+            TextRange textRange = new TextRange(rtb.Document.ContentStart, rtb.Document.ContentEnd);
+            return textRange.Text;
+        }
+
     }
 }
